9aca2bedac26538204032f31feb5ceb4cc9c98ec
ARAVINDH.K <87661167+ARAVINDH192002@users.noreply.github.com>
2021-09-29 10:13:38 +0530

    Create Count_number.java

---
+++ Count_number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CountNumberedCell {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[][] arr=new int[n][n];
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                arr[i][j]=sc.nextInt();
+                if(arr[i][j]!=20)
+                    count++;
+            }
+        }
+        System.out.print(count);
+
+        
+        sc.close();
+    }
+}
